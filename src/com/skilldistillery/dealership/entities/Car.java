248d2b84c9c35ffa32e081f97dae6c6b6b2a66c4
Abner Bouomo <abnerbouomo@gmail.com>
2024-09-19 19:09:02 -0400

    Question A to C complete

--- src/com/skilldistillery/dealership/entities/Car.java
+++ src/com/skilldistillery/dealership/entities/Car.java
@@ -1,18 +1,66 @@
 package com.skilldistillery.dealership.entities;
 
 public class Car {
-   public String make;
-   public String model;
-   public String color;
-   public double purchasePrice;
+	private String make;
+	private String model;
+	private String color;
+	private double purchasePrice;
+	
+	public Car(String make, String model, String color, double purchasePrice) {
+		this.make = make;
+		this.model = model;
+		this.color = color;
+		this.purchasePrice = purchasePrice;
+	}
+	
+	public Car() {}
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Car [make=").append(make).append(", model=").append(model).append(", color=").append(color)
+				.append(", purchasePrice=").append(purchasePrice).append("]");
+		return builder.toString();
+	}
+	
 
-   public String getCarData() {
-      String output = "make=" + make + ", model=" + model + ", color=" + color + ", purchasePrice=" + purchasePrice;
-      return output;
-   }
+	public void displayCar() { // FIX ME: Question C.6
+		// String carData = getCarData();
+		Car carData = new Car();
+		System.out.println(carData);
+	}
 
-   public void displayCar() {
-      String carData = getCarData();
-      System.out.println(carData);
-   }
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public double getPurchasePrice() {
+		return purchasePrice;
+	}
+
+	public void setPurchasePrice(double purchasePrice) {
+		this.purchasePrice = purchasePrice;
+	}
+	
+
 }
--- src/com/skilldistillery/dealership/entities/CarLot.java
+++ src/com/skilldistillery/dealership/entities/CarLot.java
@@ -1,5 +1,5 @@
 package com.skilldistillery.dealership.entities;
 
 public class CarLot {
-
+	
 }
